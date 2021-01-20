4a12ca7d92595b4d4fbdc1dc50ab5927c6e0c83e
teirm <tribunus.vii@gmail.com>
2021-01-19 22:28:24 -0500

    CMake builds tests and they all fail -- as they should

--- src/Summer.cpp
+++ src/Summer.cpp
@@ -2,3 +2,26 @@
 
 #include <Summer.hpp>
 
+// @brief Add a value to the current value of instance 
+//
+// @param[in]   value   value to add
+//
+// @return  final value held by instance
+unsigned int Summer::add_value(unsigned int value)
+{
+    (void)value;
+    return 0;
+}
+
+// @brief Return the current value of the instance
+//
+// @return value held by the instance
+unsigned int Summer::get_current_value() const
+{
+    return 0;
+}
+
+// @brief Reset the value held by the instance
+void Summer::reset_value()
+{
+}
--- tests/summer_tests.cpp
+++ tests/summer_tests.cpp
@@ -3,5 +3,30 @@
 // 19 January 2021
 
 #define CATCH_CONFIG_MAIN
+
+#include <Summer.hpp>
+
 #include <catch2/catch_all.hpp>
 
+TEST_CASE("Instance adds value given", "[add-value]") {
+    unsigned int initial_value = 0;
+    
+    Summer sum_thing(initial_value);
+    REQUIRE(sum_thing.add_value(10) == 10);
+}
+
+TEST_CASE("Instance returns current value", "[get-value]") {
+    unsigned int initial_value = 10;
+
+    Summer sum_thing(initial_value);
+    REQUIRE(sum_thing.get_current_value() == 10);
+}
+
+TEST_CASE("Instance resets value", "[reset-value]") {
+    unsigned int initial_value = 10;
+
+    Summer sum_thing(initial_value);
+    sum_thing.reset_value();
+    sum_thing.add_value(10);
+    REQUIRE(sum_thing.get_current_value() == 10);
+}
